4dc432fd153c154a88b23ee4b84b818751e1b640
Shlomo Kalinsky <93456838+sdkkds2125@users.noreply.github.com>
2023-03-19 17:41:43 -0400

    added the entire swing implementation closes #4 this will allow you to run it as a swing application but all the code is in a swing class that is passed in in main also closes #5

--- lib/src/main/java/edu/touro/mco152/bm/App.java
+++ lib/src/main/java/edu/touro/mco152/bm/App.java
@@ -240,7 +240,7 @@ public class App {
             msg("worker is null abort...");
             return;
         }
-        worker.cancel(true);
+        worker.ui.cancelTask(true);
     }
 
     public static void startBenchmark() {
@@ -263,8 +263,8 @@ public class App {
         Gui.mainFrame.adjustSensitivity();
 
         //4. set up disk worker thread and its event handlers
-        worker = new DiskWorker();
-        worker.addPropertyChangeListener((final PropertyChangeEvent event) -> {
+        worker = new DiskWorker(new SwingUIImplementation());
+        worker.ui.addAPropertyChangeListener((final PropertyChangeEvent event) -> {
             switch (event.getPropertyName()) {
                 case "progress":
                     int value = (Integer) event.getNewValue();
@@ -285,7 +285,7 @@ public class App {
         });
 
         //5. start the Swing worker thread
-        worker.execute();
+        worker.ui.runOnOpenThread();
     }
 
     /**
